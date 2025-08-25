3ae41a53cd06dae0215799499ecc8df6e8f41775
jiseon723 <dighxxx12@gmail.com>
2025-08-25 14:03:18 +0900

    init

---
+++ src/main/java/com/example/HMPS/HmpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.HMPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HMPS/HmpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HMPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
